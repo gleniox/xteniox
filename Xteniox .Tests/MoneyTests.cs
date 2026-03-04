600e011143d8ba4c7943fac2a2fa9618032b1532
gleniox <gleniox@outlook.com>
2026-03-04 15:41:16 -0300

    First version.

---
+++ Xteniox .Tests/MoneyTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Xteniox.Tests
+{
+    [TestClass]
+    public class MoneyTests
+    {
+        [TestMethod]
+        public void ShouldConvertDecimalToInt()
+        {
+            decimal value = 123.45M;
+            var cents = value.ToCents();
+
+            Assert.AreEqual(expected: 12345, actual: cents);
+        }
+    }
+}
